d1ca2bb8ac9b8cd48fd56112adc20500c311e628
Kaido Muttik <kaits90@gmail.com>
2013-02-07 15:06:17 +0200

    esimene commit

---
+++ pages/signup.php
@@ -0,0 +1,6 @@
+<span class="gensmall">Eesnimi:&nbsp; <input class="post" type="text" name="username" size="10"> <br>
+<span class="gensmall">Perekonnanimi:&nbsp; <input class="post" type="text" name="username" size="10"> <br>
+<span class="gensmall">Email:&nbsp; <input class="post" type="text" name="username" size="10"/>
+<div>
+	<a class="btn btn-small btn-success" href="http://localhost/marketing-site/?page=signup">Esita!</a>
+</div>
